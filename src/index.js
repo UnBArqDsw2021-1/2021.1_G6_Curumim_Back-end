ed79bbae0b0fe74c980ccaa1d370135b6966c565
Francisco Emanoel Ferreira da Penha <francisco170142329@gmail.com>
2021-08-20 19:20:18 -0300

    Criando estrutura basica do node.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get("/",(req, res) =>{
+    res.json({status: true})
+
+})
+
+server.listen(3000)
