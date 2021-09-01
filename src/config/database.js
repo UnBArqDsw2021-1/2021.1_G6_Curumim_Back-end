1a5486d398482fa9382351b322236a6780325f98
Daniel Porto <danielportods@gmail.com>
2021-09-01 03:55:17 -0300

    conf(#4) Inserindo configuração inicial do db.

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,10 @@
-// configurar banco de dados
+module.exports = {
+    dialect: process.env.DB_DIALECT  || 'postgres',
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    define: {
+        timestamps: true,
+    },
+};
