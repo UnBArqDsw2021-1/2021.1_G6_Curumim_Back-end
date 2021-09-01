3032430390652192c56cb9b24c6e696411182170
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-01 13:09:17 -0300

    conf(#4) Teste de conexao do banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-import  { Router } from 'express'
+import { Router } from 'express'
+import connection from './database/index.js'
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ok: true})
+routes.get('/', async (req, res) => {
+  await connection.authenticate().then(console.log('Connection has been established successfully.')).catch((error) => console.log(error));
+  return res.json({ ok: true })
 })
 
 export default routes
