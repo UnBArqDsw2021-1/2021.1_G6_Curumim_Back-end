e27807dedf40c1d84af1a9a2d72ecae3c8fdbccc
Daniel Porto <danielportods@gmail.com>
2021-09-08 13:23:16 -0300

    conf(#4) testando o db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 import Sequelize from 'sequelize'
 import dbConfig from '../config/database'
+import Ec from '../app/models/Ec.js'
 
 const connection = new Sequelize(dbConfig);
+
+Ec.init(connection);
 
 export default connection;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import connection from './database/index.js'
+import TestController from './app/controllers/testeControlloer.js'
 
 const routes = new Router();
 
@@ -7,5 +8,7 @@ routes.get('/', async (req, res) => {
   await connection.authenticate().then(console.log('Connection has been established successfully.')).catch((error) => console.log(error));
   return res.json({ ok: true })
 })
+
+routes.post('/ec', TestController.createEC)
 
 export default routes
