e651e4709b7c8ad0c0e23a237780352c63a76f53
edsondearaujo <edsondearaujofga@gmail.com>
2021-09-12 15:26:27 -0300

    #5 Construção do corpo da middleware

--- src/app/middlewares/middleware.js
+++ src/app/middlewares/middleware.js
@@ -1,2 +1,25 @@
-// autentication
-// Nesta pasta fica as regras de autenticação
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth.config";
+import { promisify } from "util";
+
+export default async(req, res, next) => {
+    const authHeader = req.authHeader.autorization;
+
+    if(!authHeader){
+        return res.status(401).json({ error: "Token não existe." })
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try{
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+        req.adminId = decoded.id;
+
+
+        return next();
+    }catch(err){
+        return res.status(401).json({ error: "Token inválido."})
+    }
+
+}
