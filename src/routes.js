7dfb247a9ce9c977482707b368c4eda7ae4f65a7
edsondearaujo <edsondearaujofga@gmail.com>
2021-09-12 23:27:06 -0300

    #5 adicionando rota para autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import connection from './database/index.js'
 import EventController from './app/controllers/EcController.js';
 import EcController from './app/controllers/EcController.js';
 import UserController from './app/controllers/UserController.js';
+import AuthController from './app/controllers/AuthController.js';
 
 const routes = new Router();
 
 
 
 routes.post('/users', UserController.store);
+routes.post('/auth/admin', AuthController.store_admin);
 
 
 
