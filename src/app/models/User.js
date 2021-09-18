17954750102dd3d5c1e22dec3593eae9ee37ce14
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-18 15:16:09 -0300

    feat(#5) salvando password_hash
    Co-authored-by: francisco1code <francisco170142329@gmail.com>
    Co-authored-by: edsondearaujo <edsondearaujofga@gmail.com>

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -11,14 +11,18 @@ class User extends Model {
         cpf: Sequelize.STRING,
         birthday: Sequelize.DATE,
         email: Sequelize.STRING,
-        password: Sequelize.STRING,
         password_hash: Sequelize.STRING,
-      
+        password: Sequelize.VIRTUAL
       },
       {
         sequelize,
       }
       );
+      this.addHook('beforeSave', async user => {
+        if(user.password){
+          user.password_hash = await bcrypt.hash(user.password, 8)
+        }
+      })
       return this;
   }
   checkPassword(password){
