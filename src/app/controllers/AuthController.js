87ee1a3d85ca2f2134f08b527151962ee05bcf95
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-18 15:12:47 -0300

    feat(#5) Alteração do método de login + try catch

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -4,33 +4,37 @@ import User from "../models/User";
 import authConfig from "../../config/auth.config";
 
 class AuthController {
-    async store(req, res){
-        const { email, password } = req.body;
+    async login(req, res){
+        try{
+            const { email, password } = req.body;
 
-        const user = await User.findOne({ where: { email:email }})
-        
-        if(!user){
-            return res.status(401).json({error: "Usuário não existe."});
-        }
+            const user = await User.findOne({ where: { email:email }})
+            
+            if(!user){
+                return res.status(401).json({error: "Usuário não existe."});
+            }
 
-        if(!(await user.checkPassword(password))){
-            return res.status(401).json({ error: "Senha incorreta."});
-        }
+            if(!(await user.checkPassword(password))){
+                return res.status(401).json({ error: "Senha incorreta."});
+            }
 
-        const {id, name} = user;
+            const {id, name} = user;
 
-        return res.json({
-            user: {
-                id,
-                name,
-                email
-            },
+            return res.json({
+                user: {
+                    id,
+                    name,
+                    email
+                },
 
-            token: jwt.sign({ id }, authConfig.secret, {
-                expiresIn: authConfig.expiresIn,
+                token: jwt.sign({ id }, authConfig.secret, {
+                    expiresIn: authConfig.expiresIn,
+                })
+
             })
-
-        })
+        }catch(err){
+            return res.status(500).json({ error: err.stack });
+        }
     }
 }
 export default new AuthController();
