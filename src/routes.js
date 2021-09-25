bacfebe894b4ddb05415081694691483eb2e4cea
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-25 20:46:15 -0300

    feat(#17): Adiciona rota de registerChild.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
-import { Router } from 'express'
-import UserController from './app/controllers/UserController.js';
-import AuthController from './app/controllers/AuthController.js';
-import AuthMiddleware from './app/middlewares/middleware.js'
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import AuthController from './app/controllers/AuthController';
+import AuthMiddleware from './app/middlewares/middleware';
+import Adm from './app/controllers/AdmController';
 
 const routes = new Router();
 
+routes.post('/adm/child', Adm.registerChild);
 routes.post('/users', UserController.store);
 routes.post('/login', AuthController.login);
-routes.use(AuthMiddleware)
+routes.use(AuthMiddleware);
 routes.get('/list-users', UserController.list);
 
-export default routes
+export default routes;
