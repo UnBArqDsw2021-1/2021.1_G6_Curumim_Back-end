a002c9967b139c0c9f5304f8a8b12e5a43043d42
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-28 01:07:46 -0300

    feat(#11) Adiciona register para adm

--- src/app/controllers/AdmController.js
+++ src/app/controllers/AdmController.js
@@ -3,7 +3,28 @@ import User from '../models/User';
 import UserController from './UserController';
 
 class AdmController extends UserController {
-
+  async register(req, res) {
+    try {
+      const usertype = 2;
+      const {
+        name, cpf, birthday, email, password, registration,
+      } = req.body;
+      const { id } = await User.create({
+        usertype, name, cpf, birthday, email, password,
+      });
+      await Professionals.create({ id, professionalType: 'adm', registration });
+      return res.json({
+        usertype,
+        name,
+        cpf,
+        birthday,
+        email,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: err.stack });
+    }
+  }
 }
 
 export default new AdmController();
