b739a4cae888551a07959f70f1f813d3b2867a06
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-27 21:39:44 -0300

    feat(#16) add routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import UserController from './app/controllers/UserController.js';
 import AuthController from './app/controllers/AuthController.js';
-import AuthMiddleware from './app/middlewares/middleware.js'
+import AuthMiddleware from './app/middlewares/middleware.js';
+import AdmController from './app/controllers/AdmController.js';
+import TeacherController from './app/controllers/TeacherController.js';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/login', AuthController.login);
-routes.use(AuthMiddleware)
+routes.post('/login', UserController.login);
+routes.post('/register-teacher', AdmController.registerTeacher);
 routes.get('/list-users', UserController.list);
+routes.get('/list-professionals', TeacherController.list)
+routes.use(AuthMiddleware);
 
-export default routes
+export default routes;
