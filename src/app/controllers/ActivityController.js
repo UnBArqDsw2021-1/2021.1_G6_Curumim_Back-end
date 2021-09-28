9b950aca268f1e2fdb41eb1f1fe21450384d59d5
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-28 19:40:35 -0300

    feat(#20) Novos métodos e refatoração

--- src/app/controllers/ActivityController.js
+++ src/app/controllers/ActivityController.js
@@ -1,18 +1,41 @@
 import UserController from './UserController';
 import Project from '../models/Project';
 
-
 class ActivityController extends UserController {
-  async createActivity(req, res){
-    try{
-      const projectType = 'activity';
-      const {title, description, date } = req.body;
+  async list(req, res) {
+    try {
+      const projects = await Project.findAll({
+        where: { projectType: 'activity' },
+      });
+      return res.json({ projects });
+    } catch (err) {
+      return res.status(500).json({ error: err.stack });
+    }
+  }
 
-      if( req.userId === undefined){
+  async listMyActivities(req, res) {
+    try {
+      const projects = await Project.findAll({
+        where: { projectType: 'activity', fk_idProfessional: req.userId },
+      });
+      return res.json({ projects });
+    } catch (err) {
+      return res.status(500).json({ error: err.stack });
+    }
+  }
+
+  async createActivity(req, res) {
+    try {
+      const projectType = 'activity';
+      const { title, description, date } = req.body;
+
+      if (req.userId === undefined) {
         return res.status(401).json({ error: 'Acesso negado.' });
       }
 
-      const project = await Project.create({fk_idProfessional: req.userId, projectType, title, description, date})
+      const project = await Project.create({
+        fk_idProfessional: req.userId, projectType, title, description, date,
+      });
 
       return res.status(201).json({
         project,
@@ -22,15 +45,15 @@ class ActivityController extends UserController {
     }
   }
 
-  async updateActivity(req, res){
-    try{
-      const {id, updates} = req.body
+  async updateActivity(req, res) {
+    try {
+      const { id, updates } = req.body;
       const project = await Project.findByPk(id);
 
-      if( project.fk_idProfessional !== req.userId && req.usertype !== 2){
+      if (project.fk_idProfessional !== req.userId && req.usertype !== 2) {
         return res.status(401).json({ error: 'Acesso negado.' });
       }
-      const updated = await project.update(updates)
+      const updated = await project.update(updates);
 
       return res.json({
         updated,
@@ -40,18 +63,18 @@ class ActivityController extends UserController {
     }
   }
 
-  async deleteActivity(req, res){
-    try{
-      const {id} = req.body
+  async deleteActivity(req, res) {
+    try {
+      const { id } = req.body;
       const project = await Project.findByPk(id);
 
-      if( project.fk_idProfessional !== req.userId && req.usertype !== 2){
+      if (project.fk_idProfessional !== req.userId && req.usertype !== 2) {
         return res.status(401).json({ error: 'Acesso negado.' });
       }
       await project.destroy();
 
       return res.json({
-        msg: "Atividade Deletada com sucesso"
+        msg: 'Atividade Deletada com sucesso',
       });
     } catch (err) {
       return res.status(500).json({ error: err.stack });
