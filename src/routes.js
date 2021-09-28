9f38547030513bd57f518f4f930322680e137d6f
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2021-09-28 01:07:28 -0300

    feat(#11) Adiciona rotas e validação

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 import AuthController from './app/controllers/AuthController.js';
-import AuthMiddleware from './app/middlewares/middleware.js';
+import Middleware from './app/middlewares/middleware.js';
 import AdmController from './app/controllers/AdmController.js';
 import TeacherController from './app/controllers/TeacherController.js';
+import GuardianController from './app/controllers/GuardianController.js';
 
 const routes = new Router();
-
+// Unverified routes
 routes.post('/login', AuthController.login);
+routes.post('/dev/register-adm', AdmController.register);
+routes.post('/register-guardian', GuardianController.register);
+// Admin routes
+routes.use(Middleware.verifyAdm);
 routes.post('/adm/register-teacher', TeacherController.register);
 routes.get('/list-professionals', TeacherController.list);
-routes.use(AuthMiddleware);
+routes.get('/list-guardians', GuardianController.list);
+// Teacher routes
+routes.use(Middleware.verifyTeacher);
+// Guardian routes
+routes.use(Middleware.verifyGuardian);
 
 export default routes;
