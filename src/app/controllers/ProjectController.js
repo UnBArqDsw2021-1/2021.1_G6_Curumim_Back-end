59db96544f9ba3deb409a8f7f8d6fd1bdbb2b688
Daniel Porto <danielportods@gmail.com>
2021-10-01 17:45:11 -0300

    feat(#20 #13) Ajustando o padrão state.

--- src/app/controllers/ProjectController.js
+++ src/app/controllers/ProjectController.js
@@ -2,52 +2,48 @@ import ActivityController from './ActivityController';
 import EventController from './EventController';
 
 class ProjectController {
-    
-    currentType;
-    #instance;
     #activity;
     #event;
+    #currentType;
 
     constructor(){
-        if (!this.#instance){
-            this.#instance = new ProjectController();
-            this.#instance.#activity = ActivityController;
-            this.#instance.#event = EventController;
-        }else{
-            return this.#instance;
-        }   
+        this.#activity = ActivityController;
+        this.#event = EventController;
     }
 
-    changeState(req, res, next) {
-        if (req.projectType === "activity"){
+    changeState (req, res, next) {
+        
+        console.log(this.#currentType);
+        if (req.body.projectType === "activity"){
+            
             this.#currentType = this.#activity;
         }
-        else if(req.projectType === "event"){
+        else if(req.body.projectType === "event"){
             this.#currentType = this.#event;
         }
         else {
             return res.status(500).json({ msg: "Tipo de projeto inválido."});
         }
 
-        return next;
+        return next();
     }
 
     async action(req, res){
         if(this.#currentType === this.#activity){
-            switch (req.action){
-                case create:
+            switch (req.body.action){
+                case "create":
                     await this.#currentType.createActivity(req, res);
                     break;
-                case update:
+                case "update":
                     await this.#currentType.updateActivity(req, res);
                     break;
-                case listAll:
+                case "listAll":
                     await this.#currentType.listAll(req, res);
                     break;
-                case listByUser:
+                case "listByUser":
                     await this.#currentType.listMyActivities(req, res);
                     break;
-                case dell:
+                case "dell":
                     await this.#currentType.deleteActivity(req, res);
                     break;
                 default: 
@@ -55,20 +51,20 @@ class ProjectController {
             }
         }
         else if(this.#currentType === this.#event){
-            switch (req.action){
-                case create:
+            switch (req.body.action){
+                case "create":
                     await this.#currentType.createEvent(req, res);
                     break;
-                case update:
+                case "update":
                     await this.#currentType.updateEvent(req, res);
                     break;
-                case listAll:
+                case "listAll":
                     await this.#currentType.listAllEvent(req, res);
                     break;
-                case listByUser:
+                case "listByUser":
                     await this.#currentType.listMyEvents(req, res);
                     break;
-                case dell:
+                case "dell":
                     await this.#currentType.deleteEvent(req, res);
                     break;
                 default:
