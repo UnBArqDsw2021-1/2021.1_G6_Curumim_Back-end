05d7c54d4db3e9dc35789eb087b398338740f471
Daniel Porto <danielportods@gmail.com>
2021-10-07 19:57:32 -0300

    configurando cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -13,6 +14,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors);
   }
 
   routes() {
