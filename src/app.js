c3b821f1a5bba8164a4757f480600cf47e603a2c
Daniel Porto <danielportods@gmail.com>
2021-10-07 19:57:32 -0300

    configurando cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import './database';
 
 require('dotenv').config();
@@ -13,6 +14,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors);
   }
 
   routes() {
