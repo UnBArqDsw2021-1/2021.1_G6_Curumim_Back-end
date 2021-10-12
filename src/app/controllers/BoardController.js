94d5290529b05a1d2111e58e822d1fe24f65d155
Daniel Porto <danielportods@gmail.com>
2021-10-12 20:39:37 -0300

    feat(#12) Pegando anotações e projetos para o mural.
    Co-authored-by: eliseukadesh67 <eliseu.kadesh1967@gmail.com>

--- src/app/controllers/BoardController.js
+++ src/app/controllers/BoardController.js
@@ -4,21 +4,38 @@ import Anotation from '../models/Anotation';
 import BoardComposite from '../utils/BoardComposite';
 import Child from '../models/Child';
 import ClassProject from '../models/ClassProject';
+import Class from '../models/Class';
+import Sequelize from 'sequelize'
+import { select } from 'underscore';
 
 class BoardController {
   async checkBoard(req, res) {
     try {
       const { childId } = req.params;
 
-      //const anotations = Anotation.findAll({ where: { fk_idChild: childId}})
+      const anotations = await Anotation.findAll({ where: { fk_idChild: childId}})
 
       const child = await Child.findByPk(childId, {
-          include: { association: 'turma'},
-      });
-      
-      const projects = await ClassProject.findAll({ where: { fk_idClass: child.turma.id } })
+          include: { association: 'turma', }, 
+      });     
 
-      return res.status(200).json({child, projects});
+      let {Project : projects} = await Class.findByPk(child.turma.id, {
+
+        include: [
+          {
+            model: Project,
+            as: "Project",
+          },
+        ],
+        order: [
+          [Project, 'date', 'desc' ]
+        ], 
+        
+      })
+
+      projects = projects.slice(0, 5)
+
+      return res.status(200).json({child, projects, anotations});
     } catch (err) {
       return res.status(500).json({ error: err.stack });
     }
@@ -26,3 +43,4 @@ class BoardController {
 }
 
 export default new BoardController();
+
