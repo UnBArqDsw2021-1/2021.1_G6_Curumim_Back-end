cb8696ca820d8eb646bb9338c519ee8b0db55c40
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-10-12 21:17:20 -0300

    feat(#12) Removendo imports desnecessarios.

--- src/app/controllers/BoardController.js
+++ src/app/controllers/BoardController.js
@@ -1,41 +1,37 @@
 /* eslint-disable class-methods-use-this */
 import Project from '../models/Project';
 import Anotation from '../models/Anotation';
-import BoardComposite from '../utils/BoardComposite';
 import Child from '../models/Child';
-import ClassProject from '../models/ClassProject';
 import Class from '../models/Class';
-import Sequelize from 'sequelize'
-import { select } from 'underscore';
 
 class BoardController {
   async checkBoard(req, res) {
     try {
       const { childId } = req.params;
 
-      const anotations = await Anotation.findAll({ where: { fk_idChild: childId}})
+      const anotations = await Anotation.findAll({ where: { fk_idChild: childId } });
 
       const child = await Child.findByPk(childId, {
-          include: { association: 'turma', }, 
-      });     
+        include: { association: 'turma' },
+      });
 
-      let {Project : projects} = await Class.findByPk(child.turma.id, {
+      let { Project: projects } = await Class.findByPk(child.turma.id, {
 
         include: [
           {
             model: Project,
-            as: "Project",
+            as: 'Project',
           },
         ],
         order: [
-          [Project, 'date', 'desc' ]
-        ], 
-        
-      })
+          [Project, 'date', 'desc'],
+        ],
 
-      projects = projects.slice(0, 5)
+      });
 
-      return res.status(200).json({child, projects, anotations});
+      projects = projects.slice(0, 5);
+
+      return res.status(200).json({ child, projects, anotations });
     } catch (err) {
       return res.status(500).json({ error: err.stack });
     }
@@ -43,4 +39,3 @@ class BoardController {
 }
 
 export default new BoardController();
-
