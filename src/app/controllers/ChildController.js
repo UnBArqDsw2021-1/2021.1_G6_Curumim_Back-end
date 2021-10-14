36ef9b07f9f028926b2ec6d28c246de4a2b74766
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-10-14 19:24:45 -0300

    feat(#27) Corrigindo nome da tabela guardian_children

--- src/app/controllers/ChildController.js
+++ src/app/controllers/ChildController.js
@@ -15,8 +15,8 @@ class ChildController {
   async listChildrenRelations(req, res) {
     try {
       const children = await Child.findAll();
-      const guardian_child = await GuardianChild.findAll();
-      return res.json({ children, relations: guardian_child });
+      const guardian_children  = await GuardianChild.findAll();
+      return res.json({ children, relations: guardian_children  });
     } catch (err) {
       return res.status(500).json({ error: err.stack });
     }
